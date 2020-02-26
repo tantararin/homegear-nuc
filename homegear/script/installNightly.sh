8d75af2917d111e8de9d2bcc8e1d160325d4893a
hdueo <52221380+hdueo@users.noreply.github.com>
2020-02-26 09:25:35 +0100

    Update installNightly.sh

--- homegear/script/installNightly.sh
+++ homegear/script/installNightly.sh
@@ -7,12 +7,22 @@ fi
 SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
 apt-get update
-sys="debian"
-codename="buster"
-system="${sys}_${codename}"
-arch=$(dpkg --print-architecture)
+system="debian_buster"
+machine=$(uname -m)
+echo "Machine: $machine"
+if [[ $machine == "x86_64" ]]; then
+	arch="amd64"
+elif [[ $machine == "aarch64" ]]; then
+	arch="arm64"
+elif [[ $machine == "armv7l" ]]; then
+	arch="armhf"
+else
+	echo "Unknown architecture"
+	exit 1
+fi
+echo "Architecture: $arch"
 
-function download {
+function downloadModule {
 	wget https://homegear.eu/downloads/nightlies/${1} || exit 1
 }
 
@@ -29,40 +39,42 @@ cd $TEMPDIR
 rm -f homegear*.deb
 rm -f libhomegear*.deb
 
-download libhomegear-base_current_${system}_${arch}.deb
-download libhomegear-node_current_${system}_${arch}.deb
-download libhomegear-ipc_current_${system}_${arch}.deb
-download homegear_current_${system}_${arch}.deb
-download homegear-nodes-core_current_${system}_${arch}.deb
-download homegear-nodes-extra_current_${system}_${arch}.deb || exit 1
-download homegear-licensing_current_${system}_${arch}.deb || exit 1
-download homegear-easy-licensing_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-base_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-node_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/libhomegear-ipc_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/python3-homegear_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-nodes-core_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-nodes-extra_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-licensing_current_${system}_${arch}.deb || exit 1
+wget https://homegear.eu/downloads/nightlies/homegear-easy-licensing_current_${system}_${arch}.deb || exit 1
 
-# Download modules
-download homegear-homematicbidcos_current_${system}_${arch}.deb
-download homegear-homematicwired_current_${system}_${arch}.deb
-download homegear-insteon_current_${system}_${arch}.deb
-download homegear-max_current_${system}_${arch}.deb
-download homegear-philipshue_current_${system}_${arch}.deb
-download homegear-sonos_current_${system}_${arch}.deb
-download homegear-kodi_current_${system}_${arch}.deb
-download homegear-ipcam_current_${system}_${arch}.deb
-download homegear-beckhoff_current_${system}_${arch}.deb
-download homegear-knx_current_${system}_${arch}.deb
-download homegear-enocean_current_${system}_${arch}.deb
-download homegear-intertechno_current_${system}_${arch}.deb
-download homegear-rs2w_current_${system}_${arch}.deb
-download homegear-rsl_current_${system}_${arch}.deb
-download homegear-zwave_current_${system}_${arch}.deb
-download homegear-mbus_current_${system}_${arch}.deb
-download homegear-ccu_current_${system}_${arch}.deb
-download homegear-easycam_current_${system}_${arch}.deb
-download homegear-easyled_current_${system}_${arch}.deb
-download homegear-easyled2_current_${system}_${arch}.deb
-download homegear-influxdb_current_${system}_${arch}.deb
-download homegear-management_current_${system}_${arch}.deb
-download homegear-webssh_current_${system}_${arch}.deb
-download homegear-adminui_current_${system}_${arch}.deb
+downloadModule homegear-homematicbidcos_current_${system}_${arch}.deb
+downloadModule homegear-homematicwired_current_${system}_${arch}.deb
+downloadModule homegear-insteon_current_${system}_${arch}.deb
+downloadModule homegear-max_current_${system}_${arch}.deb
+downloadModule homegear-philipshue_current_${system}_${arch}.deb
+downloadModule homegear-sonos_current_${system}_${arch}.deb
+downloadModule homegear-kodi_current_${system}_${arch}.deb
+downloadModule homegear-ipcam_current_${system}_${arch}.deb
+downloadModule homegear-beckhoff_current_${system}_${arch}.deb
+downloadModule homegear-knx_current_${system}_${arch}.deb
+downloadModule homegear-enocean_current_${system}_${arch}.deb
+downloadModule homegear-intertechno_current_${system}_${arch}.deb
+downloadModule homegear-rs2w_current_${system}_${arch}.deb
+downloadModule homegear-rsl_current_${system}_${arch}.deb
+downloadModule homegear-zwave_current_${system}_${arch}.deb
+downloadModule homegear-zigbee_current_${system}_${arch}.deb
+downloadModule homegear-mbus_current_${system}_${arch}.deb
+downloadModule homegear-ccu_current_${system}_${arch}.deb
+downloadModule homegear-easycam_current_${system}_${arch}.deb
+downloadModule homegear-easyled_current_${system}_${arch}.deb
+downloadModule homegear-easyled2_current_${system}_${arch}.deb
+downloadModule homegear-influxdb_current_${system}_${arch}.deb
+downloadModule homegear-management_current_${system}_${arch}.deb
+downloadModule homegear-webssh_current_${system}_${arch}.deb
+downloadModule homegear-adminui_current_${system}_${arch}.deb
+downloadModule homegear-ui_current_${system}_${arch}.deb
 
 dpkg -i libhomegear-base_current_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
@@ -80,6 +92,12 @@ dpkg -i libhomegear-ipc_current_${system}_${arch}.deb
 if [ $? -ne 0 ]; then
 	apt-get -y -f install || exit 1
 	dpkg -i libhomegear-ipc_current_${system}_${arch}.deb || exit 1
+fi
+
+dpkg -i python3-homegear_current_${system}_${arch}.deb
+if [ $? -ne 0 ]; then
+	apt-get -y -f install || exit 1
+	dpkg -i python3-homegear_current_${system}_${arch}.deb || exit 1
 fi
 
 dpkg -i homegear_current_${system}_${arch}.deb
@@ -131,6 +149,7 @@ installModule homegear-intertechno_current_${system}_${arch}.deb
 installModule homegear-rs2w_current_${system}_${arch}.deb
 installModule homegear-rsl_current_${system}_${arch}.deb
 installModule homegear-zwave_current_${system}_${arch}.deb
+installModule homegear-zigbee_current_${system}_${arch}.deb
 installModule homegear-mbus_current_${system}_${arch}.deb
 installModule homegear-ccu_current_${system}_${arch}.deb
 installModule homegear-easycam_current_${system}_${arch}.deb
@@ -140,6 +159,7 @@ installModule homegear-influxdb_current_${system}_${arch}.deb
 installModule homegear-management_current_${system}_${arch}.deb
 installModule homegear-webssh_current_${system}_${arch}.deb
 installModule homegear-adminui_current_${system}_${arch}.deb
+installModule homegear-ui_current_${system}_${arch}.deb
 
 rm -f /etc/homegear/dh1024.pem
 rm -f /etc/homegear/homegear.key
@@ -147,4 +167,4 @@ rm -f /etc/homegear/homegear.crt
 
 cd $SCRIPTDIR
 rm -Rf $TEMPDIR
-rm -f installNightly.sh
+rm -f /InstallNightly.sh
